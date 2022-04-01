504dffdf010b7123ee3b4ae94080b1c685c00de8
Longgang <73026617+dailonggang@users.noreply.github.com>
2022-04-01 23:41:13 +0800

    层序遍历

---
+++ Binary tree/Leetcode102.py
@@ -0,0 +1,17 @@
+def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        queue = collections.deque()
+        queue.append(root)
+        res = []
+        while queue:
+            size = len(queue)
+            level = []
+            for _ in range(size):
+                cur = queue.popleft()
+                if not cur:
+                    continue
+                level.append(cur.val)
+                queue.append(cur.left)
+                queue.append(cur.right)
+            if level:
+                res.append(level)
+        return res
