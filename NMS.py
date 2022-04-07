278e66f4855dec27e1e7ba807c275b17afaf17d4
Longgang <73026617+dailonggang@users.noreply.github.com>
2022-04-07 23:37:17 +0800

    手写NMS

--- NMS.py
+++ NMS.py
@@ -7,39 +7,52 @@ boxes = np.array([[100, 100, 210, 210, 0.72],
                   [100, 100, 210, 210, 0.72],
                   [230, 240, 325, 330, 0.81],
                   [220, 230, 315, 340, 0.9]])
-print(boxes.shape)
 
 
-# 设置形参,dets为绘制的矩形框，thresh为iou
+# 设置形参,dets为绘制的矩形框，thresh为置信度
 def py_cpu_nms(dets, thresh):
-    # 分别为
+    # 分别为各个矩形框两个点的坐标值
     x1 = dets[:, 0]
-    print(x1)
     y1 = dets[:, 1]
     x2 = dets[:, 2]
     y2 = dets[:, 3]
-    areas = (y2 - y1 + 1) * (x2 - x1 + 1)
-    print(areas)
+    # 计算各个矩形框的面积
+    areas = (y2 - y1) * (x2 - x1)
+    # 每个矩形框的置信度
     scores = dets[:, 4]
+    # 用来保存最后留下来的矩形框
     keep = []
+    # argsort()用法，表示对数据进行从小到大进行排序，返回数据的索引值
+    # argsort()[::-1]对置信度进行从大到小排序，返回索引值
     index = scores.argsort()[::-1]
     while index.size > 0:
-        i = index[0]  # every time the first is the biggst, and add it directly
+        # 置信度最高的矩形框的索引
+        i = index[0]
+        # 将本次置信度最高的矩形框的索引添加到keep列表中
         keep.append(i)
 
-        x11 = np.maximum(x1[i], x1[index[1:]])  # calculate the points of overlap
-        y11 = np.maximum(y1[i], y1[index[1:]])
-        x22 = np.minimum(x2[i], x2[index[1:]])
-        y22 = np.minimum(y2[i], y2[index[1:]])
+        # 当前矩形框和剩下矩形框之间的交叉区域
+        # 求交集的两个点的坐标值
+        x11 = np.maximum(x1[i], x1[index[1:]])  # 交叉区域左上角的横坐标
+        y11 = np.maximum(y1[i], y1[index[1:]])  # 交叉区域左上角的纵坐标
+        x22 = np.minimum(x2[i], x2[index[1:]])  # 交叉区域右上角的横坐标
+        y22 = np.minimum(y2[i], y2[index[1:]])  # 交叉区域右上角的纵坐标
 
-        w = np.maximum(0, x22 - x11 + 1)  # the weights of overlap
-        h = np.maximum(0, y22 - y11 + 1)  # the height of overlap
+        # 考虑不相交的情况，取最大值
+        w = np.maximum(0, x22 - x11)  # the weights of overlap
+        h = np.maximum(0, y22 - y11)  # the height of overlap
 
+        # 计算交集
         overlaps = w * h
+        # 计算IoU,index[1:]表示从第一个框到最后
+        # IoU = 交叉区域面积/(矩形框面积 + 另一个矩形框面积 - 交叉区域面积)
         ious = overlaps / (areas[i] + areas[index[1:]] - overlaps)
 
+        # 保留IoU值比较小的矩形框，因为有可能检测到的不是同一个物体
         idx = np.where(ious <= thresh)[0]
-        index = index[idx + 1]  # because index start from 1
+        print(idx)
+        #
+        index = index[idx + 1]
 
     return keep
 
@@ -57,7 +70,7 @@ def plot_bbox(dets, c='k'):
     plt.plot([x1, x1], [y1, y2], c)
     plt.plot([x1, x2], [y2, y2], c)
     plt.plot([x2, x2], [y1, y2], c)
-    plt.title(" nms")
+    plt.title("nms")
 
 
 plt.figure(1)
